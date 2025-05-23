1d02abc77945df480365f2dd95905c39ff6fa1a9
Anand KanishkZ <jranandsharma@gmail.com>
2025-05-23 23:37:00 +0545

    Feat : Added a login page, and installed some dependencies, declared some frequently using css components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import LoginPage from './Components/LoginPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
